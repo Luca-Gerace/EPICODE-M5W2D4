7fae2407ca92ce316ce355a0ecdb691611028ba3
Luca-Gerace <geraceluca91@gmail.com>
2024-06-03 16:59:20 +0200

    add crud opration

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import fantasy from './data/fantasy.json'
-import { Container } from 'react-bootstrap';
 
 function App() {
 
   return (
     <>
       <Header />
-      <Container>
+      <div className='container-fluid px-4'>
         <AllTheBooks books={fantasy} />
-      </Container>
+      </div>
       <Footer />
     </>
   )
